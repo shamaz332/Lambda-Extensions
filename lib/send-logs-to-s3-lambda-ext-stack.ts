43956c78536f4ba7a45c37b8c718f90b3ae1f2a4
shamaz332 <shamazsaeed332@gmail.com>
2021-02-01 22:04:17 +0500

    ext working properly

--- lib/send-logs-to-s3-lambda-ext-stack.ts
+++ lib/send-logs-to-s3-lambda-ext-stack.ts
@@ -1,9 +1,23 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from "path";
 
 export class SendLogsToS3LambdaExtStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //=============================================
+
+    const aa = new lambda.LayerVersion(this, "asd", {
+      code: lambda.Code.fromAsset("lambda-layers"),
+
+    });
+    const sendfunc = new lambda.Function(this, "sendlog", {
+      code: lambda.Code.fromAsset("lambda"),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "hello.handler",
+      memorySize: 1024,
+      layers: [aa],
+    });
   }
 }
